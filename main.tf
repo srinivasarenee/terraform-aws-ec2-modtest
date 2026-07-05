b28b8f939e4060e6cdfc434074877c3a31364179
Srinivasa Rao Balam <65772960+srinivasarenee@users.noreply.github.com>
2026-07-05 12:58:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,6 +2,6 @@ module "ec2" {
   source  = "app.terraform.io/devopspractice-aws/instance/aws"
   version = "1.0.0"
   ami_id        = var.ami_id
-  instance-type = var.instance-type
+  instance-type = var.instance_type
 
 }
